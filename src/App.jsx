35e0b23de9f80e58285175a222041ffd7b7831d8
HamidAya20 <hamidaya755@gmail.com>
2024-04-30 15:00:33 +0100

    importer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // import DemoPage from "./app/Agence/page"
 
-import DemoPage from "./app/Agent/page"
+//import DemoPage from "./app/Agent/page"
+//import DemoPage from "./app/application"
 
-// import CustomDrawer from './components/customComponents/CustomDrawer';
+//import CustomDrawer from './components/customComponents/CustomDrawer';
 
 function App() {
 
   return (
-    <>
+    
     <DemoPage/>
-    {/* <CustomDrawer/> */}
-    </>
+  
   )
 }
 
